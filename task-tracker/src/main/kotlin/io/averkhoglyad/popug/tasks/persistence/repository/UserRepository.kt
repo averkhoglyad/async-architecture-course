37f5f298810a308cd81cf9f21983edf586590096
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-08-16 22:02:32 +0300

    task tracker, fix user loading

--- task-tracker/src/main/kotlin/io/averkhoglyad/popug/tasks/persistence/repository/UserRepository.kt
+++ task-tracker/src/main/kotlin/io/averkhoglyad/popug/tasks/persistence/repository/UserRepository.kt
@@ -4,12 +4,15 @@ import io.averkhoglyad.popug.tasks.persistence.entity.UserEntity
 import io.averkhoglyad.popug.tasks.persistence.entity.UserRole
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 import java.util.stream.Stream
 
 interface UserRepository : JpaRepository<UserEntity, UUID> {
 
+    @Query("SELECT count(u) FROM UserEntity u WHERE u.isActive=true AND u.role=?1")
     fun countByRole(role: UserRole): Long
+    @Query("FROM UserEntity u WHERE u.isActive=true AND u.role=?1")
     fun findByRole(role: UserRole, position: Pageable): List<UserEntity>
 
 }
